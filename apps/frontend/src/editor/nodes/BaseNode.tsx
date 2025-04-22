e1171e4374089800c576d04adb53b8a89db86561
Jakov <23292936+itsJakov@users.noreply.github.com>
2025-04-22 13:18:04 +0200

    BaseNode LogicState "on" color

--- apps/frontend/src/editor/nodes/BaseNode.tsx
+++ apps/frontend/src/editor/nodes/BaseNode.tsx
@@ -1,12 +1,14 @@
 import { forwardRef, HTMLAttributes } from "react";
 
+import { LogicState } from "../types.ts";
+
 export const BaseNode = forwardRef<
     HTMLDivElement,
-    HTMLAttributes<HTMLDivElement> & { selected?: boolean }
->(({ className, selected, ...properties }, reference) => (
+    HTMLAttributes<HTMLDivElement> & { selected?: boolean; logicState?: LogicState }
+>(({ className, selected, logicState, ...properties }, reference) => (
     <div
         ref={reference}
-        className={`relative rounded-md border bg-zinc-900 border-zinc-700 px-5 py-3 hover:ring-2  ${selected ? "ring-1 shadow-lg" : ""} ${className}`}
+        className={`relative rounded-md border ${logicState === "on" ? "bg-green-900" : "bg-zinc-900"} border-zinc-700 px-5 py-3 hover:ring-2  ${selected ? "ring-1 shadow-lg" : ""} ${className}`}
         tabIndex={0}
         {...properties}
     />
