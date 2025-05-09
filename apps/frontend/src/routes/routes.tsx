0927a1dfc17515c887978fc351d8163b5d1718a0
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2025-05-09 19:50:02 +0200

    Fix

--- apps/frontend/src/routes/routes.tsx
+++ apps/frontend/src/routes/routes.tsx
@@ -4,8 +4,9 @@ import { Counter } from "@/pages/Counter";
 import { Editor } from "@/pages/Editor.tsx";
 import { Gates } from "@/pages/Gates";
 import { UsersTest } from "@/pages/UsersTest.tsx";
-import { LoggedIn, Login, LoginCallback } from "../pages/Login.tsx";
 import { Root } from "@/Root";
+
+import { LoggedIn, Login, LoginCallback } from "../pages/Login.tsx";
 
 export const MainRoutes = createBrowserRouter([
     {
