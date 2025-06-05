de5639dde7dc077d702320de7cd8cefa129fff67
bmestric97 <boris.mestric@gmail.com>
2025-06-05 13:04:59 +0200

    Add colors to simulation bar

--- apps/frontend/src/editor/SimulatorControlBar.tsx
+++ apps/frontend/src/editor/SimulatorControlBar.tsx
@@ -1,5 +1,5 @@
 import { Pause, Play, RotateCcw, SkipForward, Square } from "lucide-react";
-import { FC, useCallback } from "react";
+import { FC, useCallback, useMemo } from "react";
 
 import { Button } from "@/components/ui/button.tsx";
 import { useEditorState } from "@/editor/editorState.ts";
@@ -10,6 +10,20 @@ export const SimulatorControlBar: FC = () => {
     const pauseSimulation = useEditorState.use.pauseSimulation();
     const continueSimulation = useEditorState.use.continueSimulation();
     const stopSimulation = useEditorState.use.stopSimulation();
+
+    const bgColor = useMemo(() => {
+        switch (simulatorState) {
+            case "stopped": {
+                return "bg-card";
+            }
+            case "paused": {
+                return "bg-orange-900";
+            }
+            case "running": {
+                return "bg-green-900";
+            }
+        }
+    }, [simulatorState]);
 
     const onPlayPause = useCallback(() => {
         switch (simulatorState) {
@@ -26,10 +40,11 @@ export const SimulatorControlBar: FC = () => {
                 break;
             }
         }
+
     }, [continueSimulation, pauseSimulation, restartSimulation, simulatorState]);
 
     return (
-        <div className="bg-card border rounded-md shadow p-1 gap-3 pointer-events-auto">
+        <div className={`${bgColor} border rounded-md shadow p-1 gap-3 pointer-events-auto`}>
             <Button variant="ghost" onClick={onPlayPause}>
                 {simulatorState === "running" ? <Pause /> : <Play />}
             </Button>
