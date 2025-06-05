0024bb84c5a4b5c8ec5189822e361d2d24b4b3e6
lmalecic <162131791+lmalecic@users.noreply.github.com>
2025-06-05 15:35:07 +0200

    Add Output node

---
+++ apps/frontend/src/editor/nodes/OutputNode.tsx
@@ -0,0 +1,30 @@
+import { Handle, NodeProps, Position } from "@xyflow/react";
+
+import { EdgeStates, LogicNode, LogicNodeData, SimulatorNode } from "@/editor/types.ts";
+
+import { BaseNode } from "./BaseNode.tsx";
+
+export type OutputNode = LogicNode<LogicNodeData, "display">;
+
+export class OutputSimulatorNode extends SimulatorNode {
+    override calculateNewState(_: LogicNodeData, _inputs: EdgeStates): EdgeStates {
+        return {
+            out: _inputs["in"],
+        };
+    }
+}
+
+export const OutputNodeComponent = ({ data, selected }: NodeProps<OutputNode>) => {
+    return (
+        <>
+            <BaseNode
+                selected={selected}
+                logicState={data.simulator?.output["out"]}
+                className="!rounded-full font-extrabold"
+            >
+                {data.simulator?.output["out"] === "on" ? "1" : "0"}
+            </BaseNode>
+            <Handle id={"in"} type={"target"} position={Position.Left} />
+        </>
+    );
+};
