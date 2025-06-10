1effa37f8789b131a911058e6bb61e9c78b41467
Jakov <23292936+itsJakov@users.noreply.github.com>
2025-06-10 12:50:49 +0200

    Show sidebar toggle

--- apps/frontend/src/pages/Editor.tsx
+++ apps/frontend/src/pages/Editor.tsx
@@ -1,17 +1,20 @@
 import { FC } from "react";
 
 import { Canvas } from "@/editor/Canvas.tsx";
+import { useEditorState } from "@/editor/editorState.ts";
 import { Menu } from "@/editor/Menu.tsx";
 import { Properties } from "@/editor/Properties.tsx";
 import { SimulatorControlBar } from "@/editor/SimulatorControlBar.tsx";
 
 export const Editor: FC = () => {
+    const showProperties = useEditorState.use.showProperties();
+
     return (
         <>
             <Canvas />
 
             <div className="fixed top-0 left-0 h-screen p-4 pt-16 flex items-start gap-3 pointer-events-none">
-                <Properties />
+                {showProperties && <Properties />}
                 <SimulatorControlBar />
             </div>
 
