f8780d207ee9aa005479ec3b24c61f1f86f8745a
Jakov <23292936+itsJakov@users.noreply.github.com>
2025-06-10 12:37:57 +0200

    Properties placeholder

--- apps/frontend/src/editor/Properties.tsx
+++ apps/frontend/src/editor/Properties.tsx
@@ -7,10 +7,17 @@ import { NodeType, REGISTERED_NODES_PROPERTIES } from "./nodes/nodes.ts";
 
 const properties = REGISTERED_NODES_PROPERTIES["properties"];
 
+const Placeholder: FC<{ text: string }> = ({ text }) => {
+    return <div className="flex-grow flex justify-center items-center opacity-50">{text}</div>;
+};
+
 export const Properties: FC = () => {
     const selectedNodes = useEditorState((s) => s.selectedNodes);
 
-    const selection = useMemo(() => selectedNodes.at(0), [selectedNodes]);
+    const selection = useMemo(
+        () => (selectedNodes.length === 1 ? selectedNodes[0] : undefined),
+        [selectedNodes],
+    );
 
     const NodeProperties = useMemo(() => {
         if (!selection) return undefined;
@@ -20,8 +27,17 @@ export const Properties: FC = () => {
 
     return (
         <div className="w-[250px] h-full bg-card border rounded-md shadow flex flex-col p-3 gap-3 pointer-events-auto">
-            {NodeProperties && <NodeProperties node={selection!} />}
-            <div className="flex-grow"></div>
+            {NodeProperties ? (
+                <>
+                    <NodeProperties node={selection!} />
+                    <div className="flex-grow"></div>
+                </>
+            ) : selectedNodes.length > 1 ? (
+                <Placeholder text={"Multiple Selection"} />
+            ) : (
+                <Placeholder text={"No Selection"} />
+            )}
+
             <NewGateButton />
         </div>
     );
